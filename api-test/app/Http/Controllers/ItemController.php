f716e7c8050f0e8824113560d7479b0e23dd4459
Allan Condiman <rjaycondiman81@gmail.com>
2023-02-22 23:06:00 +0800

    Push Creating Laravel Project Test for API

---
+++ api-test/app/Http/Controllers/ItemController.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Models\Item;
+
+class ItemController extends Controller
+{
+    public function store(Request $request)
+    {
+        $postArray = $request->all();
+
+        $store_item = Item::create($postArray);
+        $success['item'] =  $store_item->item;
+        return response()->json([
+            'status' => 'success',
+            'data' => $success,
+        ]);
+    }
+}
---
+++ api-test/routes/api.php
@@ -0,0 +1,23 @@
+<?php
+
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\ItemController;
+/*
+|--------------------------------------------------------------------------
+| API Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register API routes for your application. These
+| routes are loaded by the RouteServiceProvider and all of them will
+| be assigned to the "api" middleware group. Make something great!
+|
+*/
+
+Route::prefix('items')->group(function () {
+   Route::post('', [ItemController::class, 'store']);
+});
+
+
+
+
